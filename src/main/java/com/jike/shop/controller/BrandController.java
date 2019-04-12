d19cf3d8c34bd63197d04357c002fb2320c6ddf2
txk <603085489@qq.com>
2019-04-12 10:33:05 +0800

    做的第一次提交

---
+++ src/main/java/com/jike/shop/controller/BrandController.java
@@ -0,0 +1,23 @@
+package com.jike.shop.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.jike.goods.service.BrandService;
+import com.jike.pojo.TbBrand;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/brand")
+@RestController
+public class BrandController {
+
+    @Reference
+    BrandService brandService;
+
+    @RequestMapping("/find")
+    public List<TbBrand> find(){
+        List<TbBrand> brandList = brandService.findAll();
+        return brandList;
+    }
+}
